878ac4a5f0aa421b7b5ac4a3e26d82bc710b42ae
Josse <jossjb865@gmail.com>
2026-08-02 14:19:14 -0600

    Delete pipelines/init.py

--- pipelines/init.py
+++
@@ -1,5 +0,0 @@
-from .training_pipeline import TrainingPipeline
-from .inference_pipeline import InferencePipeline
-from .roi_simulator import ROISimulator
-
-__all__ = ["TrainingPipeline", "InferencePipeline", "ROISimulator"]
